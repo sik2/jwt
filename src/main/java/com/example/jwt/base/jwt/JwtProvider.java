ef2cc8d3000320313d162854f6ced773fd3d9d95
sik2 <bickwang123@gmail.com>
2023-08-03 11:22:52 +0900

    update jwt

--- src/main/java/com/example/jwt/base/jwt/JwtProvider.java
+++ src/main/java/com/example/jwt/base/jwt/JwtProvider.java
@@ -1,11 +1,16 @@
 package com.example.jwt.base.jwt;
 
+import com.example.jwt.util.Ut;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtProvider {
@@ -23,5 +28,40 @@ public class JwtProvider {
         if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
 
         return cachedSecretKey;
+    }
+
+    public String genToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Ut.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public boolean verify(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Map<String, Object> getClaims(String token) {
+        String body = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("body", String.class);
+
+        return Ut.json.toMap(body);
     }
 }
--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -55,4 +57,53 @@ class JwtTests {
 
 		assertThat(secretKey1 == secretKey2).isTrue();
 	}
+
+	@Test
+	@DisplayName("accessToken 을 얻는다.")
+	void t5() {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("id", 1L);
+		claims.put("username", "admin");
+
+		// 지금으로부터 5시간의 유효기간을 가지는 토큰을 생성
+		String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+
+		System.out.println("accessToken : " + accessToken);
+
+		assertThat(accessToken).isNotNull();
+	}
+
+	@Test
+	@DisplayName("accessToken 을 통해서 claims 를 얻을 수 있다.")
+	void t6() {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("id", 1L);
+		claims.put("username", "admin");
+
+		// 지금으로부터 5시간의 유효기간을 가지는 토큰을 생성
+		String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+
+		System.out.println("accessToken : " + accessToken);
+
+		assertThat(jwtProvider.verify(accessToken)).isTrue();
+
+		Map<String, Object> claimsFromToken = jwtProvider.getClaims(accessToken);
+		System.out.println("claimsFromToken : " + claimsFromToken);
+	}
+
+	@Test
+	@DisplayName("만료된 토큰을 유효하지 않다.")
+	void t7() {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("id", 1L);
+		claims.put("username", "admin");
+
+		// 지금으로부터 5시간의 유효기간을 가지는 토큰을 생성
+		String accessToken = jwtProvider.genToken(claims, -1);
+
+		System.out.println("accessToken : " + accessToken);
+
+		assertThat(jwtProvider.verify(accessToken)).isFalse();
+	}
+
 }
